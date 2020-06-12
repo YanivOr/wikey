e58dbaa36799c8a2ad7d873b86c02983b85be7e9
Yaniv Or <yaniv.or.78@gmail.com>
2020-06-13 00:01:19 +0300

    added mdns

--- accessPoint.ino
+++ accessPoint.ino
@@ -5,4 +5,8 @@ void setupAccessPoint() {
   IPAddress myIP = WiFi.softAPIP();
   debug("AP IP address: ");
   debugln(myIP.toString());
+  if (!MDNS.begin("esp8266")) {
+    debugln("Error setting up MDNS responder!");
+  }
+  debugln("mDNS responder started");
 }
--- wikey.ino
+++ wikey.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include <EEPROM.h>
 #include <WebSocketsServer.h>
 #include <WebSocketsClient.h>
